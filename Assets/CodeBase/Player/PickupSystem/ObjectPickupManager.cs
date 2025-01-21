af0180e9631321417a968e5185c4d26b27d8521e
Vadim <kripesk2013@yandex.ru>
2025-01-21 18:14:14 +0300

    *ObjectPicker refactor

--- Assets/CodeBase/Player/PickupSystem/ObjectPickupManager.cs
+++ Assets/CodeBase/Player/PickupSystem/ObjectPickupManager.cs
@@ -6,79 +6,80 @@ namespace CodeBase.Player.PickupSystem
 {
     public sealed class ObjectPickupManager : MonoBehaviour
     {
+        [Header("References")]
         [SerializeField] private Camera _mainCamera;
         [SerializeField] private Button _pickupButton;
         [SerializeField] private TMP_Text _buttonText;
         [SerializeField] private Button _carHoldButton;
         [SerializeField] private Transform _holdPosition;
         [SerializeField] private Collider _truckZoneCollider;
-        [SerializeField] private float _smoothMoveSpeed = 5f;
-        [SerializeField] private float _rotationSpeed = 10f;
         [SerializeField] private Transform[] _storagePositions;
 
-        private IInteractable _currentInteractable = null;
+        [Header("Settings")]
+        [SerializeField] private float _smoothMoveSpeed = 5f;
+        [SerializeField] private float _rotationSpeed = 10f;
+
+        private IInteractable _currentInteractable;
         private Rigidbody _heldObjectRigidbody;
 
-        private void Start()
+        private void Awake()
         {
-            if (_mainCamera == null)
-                _mainCamera = Camera.main;
-            
-            
-            if (_buttonText == null)
-                _buttonText = _pickupButton.GetComponentInChildren<TMP_Text>();
-
-            _pickupButton.gameObject.SetActive(false);
-            _carHoldButton.gameObject.SetActive(false);
-            _pickupButton.onClick.AddListener(OnPickupButtonClicked);
-            _carHoldButton.onClick.AddListener(PlaceInTruck);
+            InitializeReferences();
+            BindButtonActions();
         }
 
         private void Update()
         {
-            // Проверка касания экрана для телефона, на компьютере не работает, но по тестовому нужно всё делать под телефон
-            if (Input.touchCount > 0)
-            {
-                Touch touch = Input.GetTouch(0);
-            
-                Ray touchRay = _mainCamera.ScreenPointToRay(touch.position);
+            HandleInput();
+            HandleRaycast();
+        }
 
-                RaycastHit Touchhit;
-                if (Physics.Raycast(touchRay, out Touchhit, 100f))
-                {
-                    if (!Touchhit.collider.TryGetComponent(out IInteractable interactable))
-                        return;
-                    
-                    if (_currentInteractable == null)
-                    {
-                        _currentInteractable = interactable;
-                        PickupObject();
-                    }
-                }
-            }
+        private void FixedUpdate()
+        {
+            MoveHeldObject();
+        }
 
+        private void InitializeReferences()
+        {
+            _mainCamera ??= Camera.main;
+            _buttonText ??= _pickupButton.GetComponentInChildren<TMP_Text>();
+            _pickupButton.gameObject.SetActive(false);
+            _carHoldButton.gameObject.SetActive(false);
+        }
+
+        private void BindButtonActions()
+        {
+            _pickupButton.onClick.AddListener(HandlePickupButtonClick);
+            _carHoldButton.onClick.AddListener(PlaceInTruck);
+        }
+
+        private void HandleInput()
+        {
 #if UNITY_EDITOR
             if (Input.GetMouseButtonDown(0))
-            {
-                var touch = Input.mousePosition;
-            
-                Ray touchRay = _mainCamera.ScreenPointToRay(touch);
+                ProcessTouch(Input.mousePosition);
+#else
+            if (Input.touchCount > 0)
+                ProcessTouch(Input.GetTouch(0).position);
+#endif
+        }
 
-                RaycastHit Touchhit;
-                if (Physics.Raycast(touchRay, out Touchhit, 50f))
+        private void ProcessTouch(Vector3 touchPosition)
+        {
+            Ray ray = _mainCamera.ScreenPointToRay(touchPosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 100f) && hit.collider.TryGetComponent(out IInteractable interactable))
+            {
+                if (_currentInteractable == null)
                 {
-                    if (!Touchhit.collider.TryGetComponent(out IInteractable interactable))
-                        return;
-                    
-                    if (_currentInteractable == null)
-                    {
-                        _currentInteractable = interactable;
-                        PickupObject();
-                    }
+                    _currentInteractable = interactable;
+                    PickupObject();
                 }
             }
-#endif
-            
+        }
+
+        private void HandleRaycast()
+        {
             Ray ray = _mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
             Debug.DrawRay(ray.origin, ray.direction * 100f, Color.green);
 
@@ -88,8 +89,7 @@ namespace CodeBase.Player.PickupSystem
                 {
                     _carHoldButton.gameObject.SetActive(true);
                 }
-                
-                if (hit.collider.TryGetComponent(out IInteractable interactable))
+                else if (hit.collider.TryGetComponent(out IInteractable interactable))
                 {
                     _currentInteractable = interactable;
                     _pickupButton.gameObject.SetActive(true);
@@ -105,18 +105,21 @@ namespace CodeBase.Player.PickupSystem
             }
         }
 
-        private void FixedUpdate()
+        private void MoveHeldObject()
         {
-            if (_heldObjectRigidbody != null)
-            {
-                _heldObjectRigidbody.velocity = (_holdPosition.position - _heldObjectRigidbody.position) * _smoothMoveSpeed;
+            if (_heldObjectRigidbody == null) return;
 
-                Quaternion targetRotation = _holdPosition.rotation;
-                _heldObjectRigidbody.MoveRotation(Quaternion.Slerp(_heldObjectRigidbody.rotation, targetRotation, Time.fixedDeltaTime * _rotationSpeed));
-            }
+            _heldObjectRigidbody.velocity = (_holdPosition.position - _heldObjectRigidbody.position) * _smoothMoveSpeed;
+
+            Quaternion targetRotation = _holdPosition.rotation;
+            _heldObjectRigidbody.MoveRotation(Quaternion.Slerp(
+                _heldObjectRigidbody.rotation, 
+                targetRotation, 
+                Time.fixedDeltaTime * _rotationSpeed
+            ));
         }
 
-        private void OnPickupButtonClicked()
+        private void HandlePickupButtonClick()
         {
             if (_heldObjectRigidbody != null)
                 DropObject();
@@ -126,61 +129,61 @@ namespace CodeBase.Player.PickupSystem
 
         private void PickupObject()
         {
-            _heldObjectRigidbody = _currentInteractable.GetObject().GetComponent<Rigidbody>();
-            if (_heldObjectRigidbody != null)
-            {
-                _pickupButton.gameObject.SetActive(true);
+            GameObject interactableObject = _currentInteractable.GetObject();
 
+            if (interactableObject.TryGetComponent(out Rigidbody objectRigidbody))
+            {
+                _heldObjectRigidbody = objectRigidbody;
                 _heldObjectRigidbody.isKinematic = false;
                 _heldObjectRigidbody.useGravity = false;
-                
+
+                _pickupButton.gameObject.SetActive(true);
                 _buttonText.text = "Drop";
             }
         }
 
         private void DropObject()
         {
+            if (_heldObjectRigidbody == null) return;
+
             _heldObjectRigidbody.useGravity = true;
             _heldObjectRigidbody.isKinematic = false;
-            
+
             _buttonText.text = "Pick Up";
 
             _heldObjectRigidbody = null;
             _currentInteractable = null;
         }
 
+        private void PlaceInTruck()
+        {
+            if (_heldObjectRigidbody == null) return;
+
+            foreach (var storagePosition in _storagePositions)
+            {
+                if (storagePosition.childCount > 0) continue;
+
+                _heldObjectRigidbody.transform.SetPositionAndRotation(
+                    storagePosition.position,
+                    storagePosition.rotation
+                );
+
+                _heldObjectRigidbody.isKinematic = false;
+                _heldObjectRigidbody.useGravity = true;
+                _heldObjectRigidbody.transform.SetParent(null);
+
+                _carHoldButton.gameObject.SetActive(false);
+
+                _heldObjectRigidbody = null;
+                _currentInteractable = null;
+                break;
+            }
+        }
+
         private void HidePickupButtonIfNotHolding()
         {
             if (_heldObjectRigidbody == null)
-            {
                 _pickupButton.gameObject.SetActive(false);
-            }
-        }
-        
-        private void PlaceInTruck()
-        {
-            if (_storagePositions.Length > 0)
-            {
-                foreach (var storagePosition in _storagePositions)
-                {
-                    if (storagePosition.childCount == 0)
-                    {
-                        _heldObjectRigidbody.transform.position = storagePosition.position;
-                        _heldObjectRigidbody.transform.rotation = storagePosition.rotation;
-
-                        _heldObjectRigidbody.isKinematic = false;
-                        _heldObjectRigidbody.useGravity = true;
-
-                        _heldObjectRigidbody.transform.SetParent(null);
-                        _heldObjectRigidbody = null;
-
-                        _carHoldButton.gameObject.SetActive(false);
-                        _currentInteractable = null;
-
-                        break;
-                    }
-                }
-            }
         }
     }
 }
